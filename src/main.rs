315db559fa203c9ba5f1140136f5b64f5f1b447e
Danial Chitnis <danchitnis@gmail.com>
2024-03-05 02:29:38 +0000

    RST works

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,41 @@
+use byteorder::{ByteOrder, LittleEndian};
 use futures_lite::future::block_on;
 use nusb;
-use nusb::transfer::{ControlOut, ControlType, Recipient};
+//use nusb::transfer::{ControlOut, ControlType, Recipient, RequestBuffer};
+
+const USBTMC_MSGID_DEV_DEP_MSG_OUT: u8 = 1;
+const USBTMC_MSGID_DEV_DEP_MSG_IN: u8 = 2;
+const NUMBER_OF_HEADER_BYTES: usize = 12;
+
+fn pack_bulk_out_header(msgid: u8) -> Vec<u8> {
+    const last_btag: u8 = 0x00;
+    let btag: u8 = (last_btag % 255) + 1;
+    //last_btag = btag;
+
+    // BBBx
+    vec![msgid, btag, !btag & 0xFF, 0x00]
+}
+
+fn little_write_u32(size: u32, len: u8) -> Vec<u8> {
+    let mut buf = vec![0; len as usize];
+    LittleEndian::write_u32(&mut buf, size);
+
+    buf
+}
+
+fn pack_dev_dep_msg_out_header(transfer_size: usize, eom: bool) -> Vec<u8> {
+    let mut hdr = pack_bulk_out_header(USBTMC_MSGID_DEV_DEP_MSG_OUT);
+
+    hdr.append(&mut little_write_u32(transfer_size as u32, 4));
+    hdr.push(if eom { 0x01 } else { 0x00 });
+    hdr.append(&mut vec![0x00; 3]);
+
+    hdr
+}
 
 fn main() {
+    //let last_btag: u8 = 0x00;
+
     let message: String = String::from("Hello fellow Rustaceans!");
 
     print!("{}\n", message);
@@ -18,21 +51,40 @@ fn main() {
     let device = device_info.open().expect("failed to open device");
     let interface = device.detach_and_claim_interface(0).unwrap();
 
-    let command = b"*IDN?\n";
-    //let mut buffer = [0; 256];
+    let config = device.active_configuration().unwrap();
+
+    println!("Active configuration: {:#?}", config);
+
+    let data = b"*RST\n";
+
+    let offset: usize = 0;
+    let num: usize = data.len();
+
+    let block = &data[offset..(num - offset)];
+
+    let eom = false;
+    let size: usize = block.len();
+
+    let mut req = pack_dev_dep_msg_out_header(size, eom);
+    let mut b: Vec<u8> = block.iter().cloned().collect();
+    req.append(&mut b);
+    req.append(&mut vec![0x00; (4 - (size % 4)) % 4]);
 
     println!("Sending command");
 
-    let ok = block_on(interface.control_out(ControlOut {
-        control_type: ControlType::Vendor,
-        recipient: Recipient::Device,
-        request: 0x43,
-        value: 0x0,
-        index: 0x0,
-        data: command,
-    }))
-    .into_result()
-    .unwrap();
+    let ok = block_on(interface.bulk_out(0x02, req))
+        .into_result()
+        .unwrap();
 
     println!("ok: {:?}", ok);
-} // Add this closing curly brace
+
+    //let buffer = [0; 512];
+    /*let request_buffer = RequestBuffer::new(512);
+    let okr = block_on(interface.bulk_in(0x81, request_buffer))
+        .into_result()
+        .unwrap();
+
+    println!("okr: {:?}", okr);*/
+
+    //println!("buffer: {:?}", String::from_utf8_lossy(&buffer));
+}
