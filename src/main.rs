5a2ada060a50d3d7a4e43cccb6861ee7964a2c04
Danial Chitnis <danchitnis@gmail.com>
2024-03-05 23:24:17 +0000

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,21 @@
 
 use byteorder::{ByteOrder, LittleEndian};
 use futures_lite::future::block_on;
-use nusb::{self, transfer::RequestBuffer};
+use nusb::transfer::RequestBuffer;
 
 const USBTMC_MSGID_DEV_DEP_MSG_OUT: u8 = 1;
 const USBTMC_MSGID_DEV_DEP_MSG_IN: u8 = 2;
 
+fn little_write_u32(size: u32, len: u8) -> Vec<u8> {
+    let mut buf = vec![0; len as usize];
+    LittleEndian::write_u32(&mut buf, size);
+
+    buf
+}
+
+/*
+* USBTMC document Table 1
+*/
 fn pack_bulk_out_header(msgid: u8) -> Vec<u8> {
     let last_btag: u8 = 0x00;
     let btag: u8 = (last_btag % 255) + 1;
@@ -18,35 +28,42 @@ fn pack_bulk_out_header(msgid: u8) -> Vec<u8> {
     vec![msgid, btag, !btag & 0xFF, 0x00]
 }
 
-fn little_write_u32(size: u32, len: u8) -> Vec<u8> {
-    let mut buf = vec![0; len as usize];
-    LittleEndian::write_u32(&mut buf, size);
-
-    buf
-}
-
+/*
+* USBTMC document Table 3 and USB488 document Table 3
+*/
 fn pack_dev_dep_msg_out_header(transfer_size: usize, eom: bool) -> Vec<u8> {
-    let mut hdr = pack_bulk_out_header(USBTMC_MSGID_DEV_DEP_MSG_OUT);
+    let mut header = pack_bulk_out_header(USBTMC_MSGID_DEV_DEP_MSG_OUT);
 
-    hdr.append(&mut little_write_u32(transfer_size as u32, 4));
-    hdr.push(if eom { 0x01 } else { 0x00 });
-    hdr.append(&mut vec![0x00; 3]);
+    let mut total_transfer_size: Vec<u8> = little_write_u32(transfer_size as u32, 4);
+    let bm_transfer_attributes: u8 = if eom { 0x01 } else { 0x00 };
 
-    hdr
+    header.append(&mut total_transfer_size);
+    header.push(bm_transfer_attributes);
+    header.append(&mut vec![0x00; 3]);
+
+    // check if transfer size is not 0
+
+    header
 }
 
+/*
+* USBTMC document Table 4
+*/
 fn pack_dev_dep_msg_in_header(transfer_size: usize, term_char: u8) -> Vec<u8> {
-    let mut hdr = pack_bulk_out_header(USBTMC_MSGID_DEV_DEP_MSG_IN);
+    let mut header = pack_bulk_out_header(USBTMC_MSGID_DEV_DEP_MSG_IN);
 
-    hdr.append(&mut little_write_u32(transfer_size as u32, 4));
-    hdr.push(if term_char == 0 { 0x00 } else { 0x02 });
-    hdr.push(term_char);
-    hdr.append(&mut vec![0x00; 2]);
+    let mut max_transfer_size: Vec<u8> = little_write_u32(transfer_size as u32, 4);
+    let bm_transfer_attributes: u8 = if term_char == 0 { 0x00 } else { 0x02 };
 
-    hdr
+    header.append(&mut max_transfer_size);
+    header.push(bm_transfer_attributes);
+    header.push(term_char);
+    header.append(&mut vec![0x00; 2]);
+
+    header
 }
 
-fn is_command(data: &[u8]) -> bool {
+fn is_query(data: &[u8]) -> bool {
     // Define the pattern you are looking for
     let pattern = b"?\n";
 
@@ -86,7 +103,7 @@ fn main() {
 
     let block = &data[offset..(num - offset)];
 
-    let eom = false;
+    let eom = true;
     let size: usize = block.len();
 
     let mut req = pack_dev_dep_msg_out_header(size, eom);
@@ -102,9 +119,9 @@ fn main() {
 
     println!("ok: {:?}", ok);
 
-    let command = is_command(data);
+    let query = is_query(data);
 
-    if command {
+    if query {
         println!("Command detected");
 
         let max_transfer_size: usize = 1024 * 1024;
@@ -123,12 +140,31 @@ fn main() {
             .unwrap();
 
         println!("okr: {:?}", okr);
+        println!();
 
         // Convert the numeric values to a String
-        let ascii_string: String = okr.iter().map(|&c| c as char).collect();
+        //let ascii_string: String = okr.iter().map(|&c| c as char).collect();
 
-        // Print the ASCII string to the terminal
-        println!("ASCII String: {}", ascii_string);
+        // Separate the bytes
+        let (header, payload) = okr.split_at(12);
+
+        // Convert the first part to hexadecimal values and print
+        print!("Hex values: ");
+        for byte in header {
+            print!("{:02x} ", byte);
+        }
+        println!(); // Add a new line for clarity
+
+        let payload_size = LittleEndian::read_u32(&header[4..8]) as usize;
+        println!("Payload size: {}", payload_size);
+
+        let eom = (header[8] & 0x01) != 0;
+        println!("EOM: {}", eom);
+
+        // Convert the second part to ASCII and print
+        print!("ASCII values: ");
+        let ascii_string = String::from_utf8(payload.to_vec()).unwrap();
+        println!("{}", ascii_string);
     } else {
         println!("No command detected. exiting.");
     }
