aabd0912f0e0cd1f4603e8d6fad487865f4c50e8
Danial Chitnis <danchitnis@gmail.com>
2024-03-08 14:15:57 +0000

    works with big data

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ fn main() {
 
     print!("{}\n", message);
 
-    let device_all = nusb::list_devices().unwrap();
-    println!("{:#?}", device_all.collect::<Vec<_>>()); // Collect the iterator into a Vec and print it with pretty formatting
+    //let device_all = nusb::list_devices().unwrap();
+    //println!("{:#?}", device_all.collect::<Vec<_>>()); // Collect the iterator into a Vec and print it with pretty formatting
 
     let device_info: nusb::DeviceInfo = nusb::list_devices()
         .unwrap()
@@ -19,19 +19,23 @@ fn main() {
     let device: nusb::Device = device_info.open().expect("failed to open device");
     let mut interface: nusb::Interface = device.detach_and_claim_interface(0).unwrap();
 
-    let config: nusb::descriptors::Configuration<'_> = device.active_configuration().unwrap();
+    //let config: nusb::descriptors::Configuration<'_> = device.active_configuration().unwrap();
 
-    println!("Active configuration: {:#?}", config);
+    //println!("Active configuration: {:#?}", config);
 
     usbtmc::send_command(&mut interface, "*IDN?");
 
     usbtmc::send_command(&mut interface, ":CHANnel1:SCALe?");
 
+    usbtmc::send_command(&mut interface, ":TIMebase:MODE MAIN");
+
+    usbtmc::send_command(&mut interface, ":WAVeform:POINts:MODE RAW");
+
     usbtmc::send_command(&mut interface, ":DIGitize CHANnel1");
 
     usbtmc::send_command(&mut interface, ":WAVeform:FORMat BYTE");
 
-    usbtmc::send_command(&mut interface, ":WAVeform:POINts 1000");
+    usbtmc::send_command(&mut interface, ":WAVeform:POINts 2675");
 
     usbtmc::send_command(&mut interface, ":WAVeform:DATA?");
 }
--- src/usbtmc.rs
+++ src/usbtmc.rs
@@ -9,6 +9,26 @@ use nusb::transfer::RequestBuffer;
 const USBTMC_MSGID_DEV_DEP_MSG_OUT: u8 = 1;
 const USBTMC_MSGID_DEV_DEP_MSG_IN: u8 = 2;
 
+fn print_array(bytes: Vec<u8>) {
+    let len = bytes.len();
+    if len <= 25 {
+        for byte in &bytes {
+            print!("{} ", byte);
+        }
+        println!(); // Print a newline for neatness
+    } else {
+        print!("[");
+        for byte in bytes.iter().take(20) {
+            print!("{} ", byte);
+        }
+        print!(" ... ");
+        for byte in bytes.iter().skip(len - 5) {
+            print!("{} ", byte);
+        }
+        println!("]");
+    }
+}
+
 fn little_write_u32(size: u32, len: u8) -> Vec<u8> {
     let mut buf = vec![0; len as usize];
     LittleEndian::write_u32(&mut buf, size);
@@ -19,8 +39,8 @@ fn little_write_u32(size: u32, len: u8) -> Vec<u8> {
 /*
 * USBTMC document Table 1
 */
-fn pack_bulk_out_header(msgid: u8, last_btag: u8) -> Vec<u8> {
-    //let last_btag: u8 = 0x00;
+fn pack_bulk_out_header(msgid: u8) -> Vec<u8> {
+    let last_btag: u8 = 0x00;
     let btag: u8 = (last_btag % 255) + 1;
     //last_btag = btag;
 
@@ -32,7 +52,7 @@ fn pack_bulk_out_header(msgid: u8, last_btag: u8) -> Vec<u8> {
 * USBTMC document Table 3 and USB488 document Table 3
 */
 fn pack_dev_dep_msg_out_header(transfer_size: usize, eom: bool) -> Vec<u8> {
-    let mut header = pack_bulk_out_header(USBTMC_MSGID_DEV_DEP_MSG_OUT, 0x00);
+    let mut header = pack_bulk_out_header(USBTMC_MSGID_DEV_DEP_MSG_OUT);
 
     let mut total_transfer_size: Vec<u8> = little_write_u32(transfer_size as u32, 4);
     let bm_transfer_attributes: u8 = if eom { 0x01 } else { 0x00 };
@@ -49,8 +69,8 @@ fn pack_dev_dep_msg_out_header(transfer_size: usize, eom: bool) -> Vec<u8> {
 /*
 * USBTMC document Table 4
 */
-fn pack_dev_dep_msg_in_header(transfer_size: usize, term_char: u8, index: u8) -> Vec<u8> {
-    let mut header = pack_bulk_out_header(USBTMC_MSGID_DEV_DEP_MSG_IN, index);
+fn pack_dev_dep_msg_in_header(transfer_size: usize, term_char: u8) -> Vec<u8> {
+    let mut header = pack_bulk_out_header(USBTMC_MSGID_DEV_DEP_MSG_IN);
 
     let mut max_transfer_size: Vec<u8> = little_write_u32(transfer_size as u32, 4);
     let bm_transfer_attributes: u8 = if term_char == 0 { 0x00 } else { 0x02 };
@@ -64,46 +84,69 @@ fn pack_dev_dep_msg_in_header(transfer_size: usize, term_char: u8, index: u8) ->
 }
 
 fn is_query(data: &[u8]) -> bool {
-    // Define the pattern you are looking for
-    let pattern = b"?\n";
+    // Define the byte you are looking for
+    let question_mark = b'?';
 
-    // Use the ends_with method to check if the data ends with the pattern
-    data.ends_with(pattern)
+    // Use the contains method to check if the data contains the byte
+    data.contains(&question_mark)
 }
 
-fn read_data(interface: &mut nusb::Interface, index: u8) -> (bool, usize) {
-    let max_transfer_size: usize = 1024 * 1024;
+fn read_data_transfer(
+    interface: &mut nusb::Interface,
+    buffer_size: usize,
+    big_buffer: &mut Vec<u8>,
+) -> usize {
+    let request_buffer = RequestBuffer::new(buffer_size);
+    let okr = block_on(interface.bulk_in(0x81, request_buffer))
+        .into_result()
+        .unwrap();
 
-    let send = pack_dev_dep_msg_in_header(max_transfer_size, 0, index);
+    print!("okr->: ");
+    print_array(okr.to_vec());
+
+    big_buffer.extend_from_slice(&okr);
+
+    let usb_packet_size = okr.len();
+    println!("usb packet size: {}", usb_packet_size);
+
+    usb_packet_size
+}
+
+fn read_data(
+    interface: &mut nusb::Interface,
+    big_big_buffer: &mut Vec<u8>,
+    req_buffer_size: Option<usize>,
+) -> bool {
+    let max_transfer_size: usize = 1024 * 1024;
+    let buffer_size: usize;
+    let mut big_buffer: Vec<u8> = Vec::new();
+
+    match req_buffer_size {
+        Some(size) => {
+            if size > max_transfer_size {
+                panic!("Requested buffer size is greater than max transfer size");
+            }
+            buffer_size = req_buffer_size.unwrap();
+        }
+        None => {
+            buffer_size = 1024 as usize;
+        }
+    }
+
+    let send = pack_dev_dep_msg_in_header(max_transfer_size, 0);
     let ok2 = block_on(interface.bulk_out(0x02, send))
         .into_result()
         .unwrap();
 
     println!("ok2->: {:?}", ok2);
 
-    //let buffer = [0; 512];
-    let request_buffer = RequestBuffer::new(20000);
-    let okr = block_on(interface.bulk_in(0x81, request_buffer))
-        .into_result()
-        .unwrap();
-
-    println!("okr<-: {:?}", okr);
-    println!("PAYLOAD");
-
-    let usb_packet_size = okr.len();
-    println!("usb packet size: {}", usb_packet_size);
-
-    let term_recv: bool = okr[okr.len() - 1] == 0x0A; //=10
-    println!("term_recv: {}", term_recv);
-
-    // Convert the numeric values to a String
-    //let ascii_string: String = okr.iter().map(|&c| c as char).collect();
+    let mut usb_packet_recv_size = read_data_transfer(interface, buffer_size, &mut big_buffer);
 
     // Separate the bytes
-    let (header, payload): (&[u8], &[u8]) = okr.split_at(12);
+    let (header, _): (&[u8], &[u8]) = big_buffer.split_at(12);
 
     // Convert the first part to hexadecimal values and print
-    print!("Hex values: ");
+    print!("Header Hex values: ");
     for byte in header {
         print!("{:02x} ", byte);
     }
@@ -115,12 +158,14 @@ fn read_data(interface: &mut nusb::Interface, index: u8) -> (bool, usize) {
     let eom: bool = header[8] == 0x01;
     println!("EOM: {}", eom);
 
-    // Convert the second part to ASCII and print
-    //print!("ASCII values: ");
-    //let ascii_string = String::from_utf8(payload.to_vec()).unwrap();
-    //println!("{}", ascii_string);
+    while usb_packet_recv_size == buffer_size {
+        println!("Packet size equals buffer size. Reading more data.");
+        usb_packet_recv_size = read_data_transfer(interface, buffer_size, &mut big_buffer);
+    }
 
-    (eom, usb_packet_size)
+    big_big_buffer.extend_from_slice(&big_buffer[12..]);
+
+    eom
 }
 
 pub fn send_command(interface: &mut nusb::Interface, command: &str) {
@@ -150,12 +195,28 @@ pub fn send_command(interface: &mut nusb::Interface, command: &str) {
 
     let query = is_query(data);
 
+    let mut big_big_buffer: Vec<u8> = Vec::new();
+
     if query {
         println!("query detected");
-        let (mut eom, packet_size) = read_data(interface, 0x00);
+        let mut eom = read_data(interface, &mut big_big_buffer, None);
 
         while !eom {
-            (eom, _) = read_data(interface, 0x00);
+            println!("eom is false. Reading more data.");
+            eom = read_data(interface, &mut big_big_buffer, None);
+        }
+        println!(
+            "transfer complete. total payload size: {}",
+            big_big_buffer.len()
+        );
+        let term_recv: bool = big_big_buffer[big_big_buffer.len() - 1] == 0x0A; //=10
+        println!("term_recv: {}", term_recv);
+
+        if big_big_buffer.len() > 100 {
+            println!("The first 10 bytes of the payload:");
+            let ten_bytes = &big_big_buffer[0..10];
+            let ascii_string: String = ten_bytes.iter().map(|&b| b as char).collect();
+            println!("{}", ascii_string);
         }
     } else {
         println!("No command detected. exiting.");
